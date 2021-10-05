2153fe9b00202befdd9e6f9e77c95f7c60e98ecf
thormiwa <thormiwa04@gmail.com>
2021-10-05 21:07:40 +0100

    added changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
