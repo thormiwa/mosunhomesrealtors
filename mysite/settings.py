75eaa7b98d64f70f5b2bda1cae91aea216020dec
thormiwa <thormiwa04@gmail.com>
2021-10-06 10:00:00 +0100

    whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, "blog/static"),
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
