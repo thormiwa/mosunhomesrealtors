045f9118f6ac2ee80c937b3be1d9522be09ffa03
thormiwa <thormiwa04@gmail.com>
2021-10-09 06:05:35 +0100

    i did it

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'sslify.middleware.SSLifyMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,7 +58,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'sslify.middleware.SSLifyMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
