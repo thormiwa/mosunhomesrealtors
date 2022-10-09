2fc21367edb5028214b26188098ac4077350099b
thormiwa <thormiwa04@gmail.com>
2022-10-09 21:03:06 +0100

    new website change

--- blog/static/js/index.js
+++
@@ -1,6 +0,0 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-nav_bar-toggle');
-
-navBarToggle.addEventListener('click', function() {
-    mainNav.classList.toggle('active');
-});
